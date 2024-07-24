0ffe91a9d240727f2c575a18ab1617714b0a78c4
asjadreza <mdasjad.reza@symbtechnologies.com>
2024-07-24 20:36:12 +0530

    production level backend started

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require('dotenv').config();
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+    res.send("asjadreza");
+})
+
+app.get("/login", (req, res) => {
+    res.send("<h1>Please login before go ahead!</h1>");
+})
+
+app.get("/youtube", (req, res) => {
+    res.send("<h2>Chai aur code.</h2>");
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
